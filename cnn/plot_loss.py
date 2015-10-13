421d7fb5c25a3fe9fdaa705c2880a5e0de32c75a
leinxx <alphaleiw@gmail.com>
2015-10-12 23:39:30 -0400

    added multi-stage training

--- cnn/plot_loss.py
+++ cnn/plot_loss.py
@@ -8,8 +8,8 @@ import sys
 
 lines = open('log.txt').readlines()
 lines = [l.strip().split() for l in lines if l.find(' loss = ') != -1]
-loss = [l[-1] for l in lines]
-it = [l[-4].strip(',') for l in lines]
+loss = [l[-1] for l in lines[4:]]
+it = [l[-4].strip(',') for l in lines[4:]]
 loss = np.asarray(loss).astype(float);
 it = np.asarray(it).astype(int);
 plt.plot(it,loss)
--- cnn/predict.sh
+++ cnn/predict.sh
@@ -1,11 +1,31 @@
-
-basedir=/home/lein/sar_dnn/dataset/beaufort_2010_2011/batches_45
-model=${basedir}/model/_iter_
+# the solver used here is a concatanation of the caffe solver and caffe arch file
+# a separator is added to indicat the seperation of their contents
+# this script first seperate the combined file into two files: solver file and arch
+# then the solver and arch files will be configured/filled 
+scene=20101006
 arch=deploy.prototxt
+basedir=/home/lein/sar_dnn/dataset/beaufort_2010_2011/batches_45
 meanfile=${basedir}/mean_std.txt
+trainsource=${batchdir}/source.txt
+weights=${basedir}/model/_iter_3000.caffemodel
 imagedir=/home/lein/sar_dnn/dataset/beaufort_2010_2011/hhv
-hh=20101006-HH-8by8-mat.tif
-hv=20101006-HV-8by8-mat.tif
-image=${hh},${hv}
+featurename=fc5
+hh=${scene}-HH-8by8-mat.tif
+hv=${scene}-HV-8by8-mat.tif
+image=${imagedir}/${hh},${imagedir}/${hv}
+predictdir=${basedir}/predict
+mkdir -p ${predict}
+predict=${predictdir}/${scene}.tif
+archfill=.fill_${arch}
+cp ${arch} ${archfill}
+
+fancyDelim=$(printf '\001')
+sed -i "s${fancyDelim}\$train_mean${fancyDelim}${meanfile}${fancyDelim}g" $archfill
 gdb --args ../../caffe/build/tools/caffe_predict \
-    --image=${image} --model=${arch} --weights=${model} --predict=${predict}
+    --model=${archfill} \
+    --weights=${weights} \
+    --image=${image} \
+    --meanfile=${meanfile} \
+    --featurename=${featurename} \
+    --predict=${predict} 2>&1 | tee log.txt
+
